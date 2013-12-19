c4644a02ee329d90a7a398576e9aeca66985149f
nickgrosvenor <nick@cinegrain.com>
2013-12-18 22:14:47 -0800

    added voting opacity hot

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -7,15 +7,23 @@ class AlbumsController < ApplicationController
   #   if current_user
   # @album = Album.create!(album_params)
   #   end
-  
+        if current_user
       @album = current_user.albums.create!(album_params)
+
+      @album.photos.create(direct_upload_url: @album.direct_upload_url, rating_amount: 0)
       redirect_to :back
+        else
+        redirect_to root_url 
+        
+  
+        end
   end
 
 
   def show
   @album = Album.find(params[:id])
   @photo = Photo.new
+
 
   end   
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   def index
+    @album = Album.new
+    @all_albums = Album.all
   end
 
   def new
@@ -9,7 +11,7 @@ class UsersController < ApplicationController
 
   def home
     @album = Album.new
-    @all_albums = Album.all
+    @all_albums = Album.find_by_sql("SELECT a.id, a.name, SUM(p.rating_amount) AS \"rating\" FROM albums a LEFT OUTER JOIN album_photos ap ON a.id = ap.album_id LEFT OUTER JOIN photos p ON ap.photo_id = p.id GROUP BY a.id, a.name ORDER BY SUM(p.rating_amount) DESC")
   end
 
   def create
